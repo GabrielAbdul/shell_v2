e0a85dd1f6f1646d645a8c9e8a9d878c9c941d8d
Gabriel <Gabrielabdul1@gmail.com>
2021-08-15 14:58:30 +0000

    corrected error printing

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -7,7 +7,7 @@
  */
 int myexit(char **args)
 {
-	int status;
+	int status, i;
 
 	if (args[1] == NULL)
 	{
@@ -15,6 +15,12 @@ int myexit(char **args)
 		return (0);
 	}
 
+	for (i = 0; args[1][i]; i++)
+		if (args[1][i] < 48 || args[1][i] > 57)
+		{
+			_dprintf(STDERR_FILENO, "hsh: %d: Illegal number: %s\n", line_no, args[1]);
+			break;
+		}
 	running = false;
 	status = atoi(args[1]);
 	return (status);
